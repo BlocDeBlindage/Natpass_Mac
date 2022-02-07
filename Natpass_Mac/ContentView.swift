4d5f49d230b893b8d14ac14b198a51d4c6bb37f4
BlocDeBlindage <gravejatnathael@gmail.com>
2022-02-07 16:29:36 +0100

    Initial Commit

---
+++ Natpass_Mac/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Natpass_Mac
+//
+//  Created by etudiant btssnir on 07/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Natpass_Mac/Natpass_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  Natpass_MacApp.swift
+//  Natpass_Mac
+//
+//  Created by etudiant btssnir on 07/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Natpass_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
