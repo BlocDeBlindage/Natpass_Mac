ed192c2c97053833feaa78cf882b52218d410a79
BlocDeBlindage <gravejatnathael@gmail.com>
2022-03-15 09:38:25 +0100

    début json to swift

---
+++ Natpass_Mac/JSON.swift
@@ -0,0 +1,14 @@
+//
+//  JSON.swift
+//  Natpass_Mac
+//
+//  Created by etudiant btssnir on 15/03/2022.
+//
+
+import SwiftUI
+import Foundation
+
+class JSON: Codable, Identifiable {
+    var email: String
+    var password: String
+}
